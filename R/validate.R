e84ee57ad9ac58ad40915ff261ace3c256736003
Jeroen Ooms <jeroenooms@gmail.com>
2015-01-10 19:05:14 -0800

    Add validation function

---
+++ R/validate.R
@@ -0,0 +1,17 @@
+#' Validate JavaScript
+#'
+#' Simple wrapper for \code{ct$validate} in \code{\link{V8}}.
+#'
+#' @export
+#' @param text character vector with JavaScript
+#' @examples validate_js("function foo(x){2*x}") #TRUE
+#' validate_js("foo = function(x){2*x}") #TRUE
+#'
+#' # Anonymous functions in global scope are invalid
+#' validate_js("function(x){2*x}") #FALSE
+#'
+#' # But as argument is OK
+#' validate_js("test(function(x){2*x})") #TRUE
+validate_js <- function(text){
+  ct$validate(text)
+}
