d5ceee6fee36c97de5ee047dd7ee5fa85dc7ce68
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-19 14:10:11 -0800

    Add coffee script.

---
+++ R/coffee.R
@@ -0,0 +1,16 @@
+#' Coffee Script
+#'
+#' Compiles coffee script into JavaScript.
+#'
+#' @export
+#' @aliases coffee
+#' @param code a string with JavaScript code
+#' @param opts options passed to the compiler
+#' @examples demo <- readLines(system.file("example/demo.coffee", package = "js"))
+#' js <- coffee_compile(demo)
+#' cat(js)
+#' cat(uglify_optimize(js))
+coffee_compile <- function(code, opts = list()){
+  code <- paste(code, collapse = "\n")
+  ct$call("coffee.compile", code, opts)
+}
