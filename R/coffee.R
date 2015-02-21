aed18c43879b20a65e0c1ccfa2fefd0e7c634806
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-21 00:14:24 -0800

    Add coffee examples

--- R/coffee.R
+++ R/coffee.R
@@ -6,7 +6,12 @@
 #' @aliases coffee
 #' @param code a string with JavaScript code
 #' @param ... additional options passed to the compiler
-#' @examples demo <- readLines(system.file("example/demo.coffee", package = "js"))
+#' @examples # Hello world
+#' coffee_compile("square = (x) -> x * x")
+#' coffee_compile("square = (x) -> x * x", bare = TRUE)
+#'
+#' # Simple script
+#' demo <- readLines(system.file("example/demo.coffee", package = "js"))
 #' js <- coffee_compile(demo)
 #' cat(js)
 #' cat(uglify_optimize(js))
